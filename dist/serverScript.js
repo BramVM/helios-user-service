9c9fae1544b0b09ab09fb21b4c20c101a03858a0
Bram Van Mensel <bram.vanmensel@nxtport.eu>
2020-05-13 17:17:10 +0200

    cors fix attempt

--- dist/serverScript.js
+++ dist/serverScript.js
@@ -353,16 +353,18 @@ const corsOptions = {
   credentials: true,
   enablePreflight: true
 };
-app.use(cors(corsOptions));
-
 //app.use(cors({origin: 'http://localhost:3000'}));
 app.use(jwtCheck);
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 });
 
 app.listen(port);
