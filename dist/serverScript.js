cb7fc272e50e666cacf6faf152a2357f6f965ff5
Bram Van Mensel <bram.vanmensel@nxtport.eu>
2020-05-13 17:45:18 +0200

    fix cors attempt

--- dist/serverScript.js
+++ dist/serverScript.js
@@ -355,7 +355,7 @@ const corsOptions = {
 };
 //app.use(cors({origin: 'http://localhost:3000'}));
 app.use(jwtCheck);
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);
